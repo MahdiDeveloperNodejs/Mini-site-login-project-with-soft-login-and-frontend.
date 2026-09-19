4bb531c5ec63d5046662cd94d3d2148f005bc99d
mahdi zare <mohemadzare13@gmail.com>
2026-09-19 12:09:42 -0700

    add fix model and route

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    fullname: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+const UserModel = model("User", UserSchema);
+module.exports = UserModel;
